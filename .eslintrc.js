36a9649bd9456e01da52d151ae1ded769161ce8a
EnriqueRZ <jramirez348@accitesz.com>
2020-10-09 13:47:06 -0500

    Identation fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-	env: {
+  env: {
     browser: true,
     commonjs: true,
     es2020: true,
@@ -16,15 +16,15 @@ module.exports = {
   },
   plugins: [
     'react',
-    'react-native'
+    'react-native',
   ],
   rules: {
-    //"indent": [2, "tab", { "SwitchCase": 1, "VariableDeclarator": 1 }],
-    "react/prop-types": 0,
-    "no-use-before-define": ["error", { "variables": false }],
-    //"react/jsx-indent": [2, "tab"],
-    //"react/jsx-indent-props": [2, "tab"],
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "import/no-named-as-default": 0
+    // "indent": [2, "tab", { "SwitchCase": 1, "VariableDeclarator": 1 }],
+    'react/prop-types': 0,
+    'no-use-before-define': ['error', { variables: false }],
+    // "react/jsx-indent": [2, "tab"],
+    // "react/jsx-indent-props": [2, "tab"],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/no-named-as-default': 0,
   },
 };
--- app/screens/Splash.js
+++ app/screens/Splash.js
@@ -2,64 +2,63 @@ import React, { Component } from 'react';
 import {View, StyleSheet, Image, SafeAreaView} from 'react-native';
 
 export default class Splash extends Component {
-	_isMounted = false;
-
-	state = {
-    books: {}
-	};
+  state = {
+    books: {},
+    _isMounted: false,
+  };
 	
-	componentDidMount() {
-		this._isMounted = true;
-		const data = this.navigateToHome();
-	}
+  componentDidMount() {
+    this._isMounted = true;
+    const data = this.navigateToHome();
+  }
 
-	componentWillUnmount() {
+  componentWillUnmount() {
     this._isMounted = false;
-	}
+  }
 
-	navigateToHome = async () => {
-    const wait = time => new Promise((resolve) => setTimeout(resolve, time));
+  navigateToHome = async () => {
+    const wait = (time) => new Promise((resolve) => setTimeout(resolve, time));
     return wait(3000).then(() => this.props.navigation.navigate('Home'))
-	};
+  };
 
-	render() {
-		return (
-			<SafeAreaView style={styles.container}>
-				<View>
-					<Image
-						style={styles.stretch}
-						source={require('../../assets/img/logoSplash.jpg')}
-					/>
-				</View>
-				<View style={styles.containerTarasco}>
-					<Image
-						style={styles.imageTarasco}
-						source={require('../../assets/img/logoTarasco.png')}
-					/>
-				</View> 
-			</SafeAreaView> 
-		);
-	}
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Image 
+            style={styles.stretch}
+            source={require('../../assets/img/logoSplash.jpg')}
+          />
+        </View>
+        <View style={styles.containerTarasco}>
+          <Image
+            style={styles.imageTarasco}
+            source={require('../../assets/img/logoTarasco.png')}
+          />
+        </View> 
+      </SafeAreaView> 
+    );
+  }
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex : 1,
-		justifyContent: 'center',
-		backgroundColor: 'white'
-	},
-	stretch: {
-		width: 400,
-		height: 400,
-		resizeMode: 'stretch',
-	},
-	containerTarasco: {
-		paddingTop: 120,
-		alignItems: 'center',
-		backgroundColor: 'white'
-	},
-	imageTarasco: {
-		width: 150,
-		height: 31,
-	}
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  stretch: {
+    width: 400,
+    height: 400,
+    resizeMode: 'stretch',
+  },
+  containerTarasco: {
+    paddingTop: 120,
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  imageTarasco: {
+    width: 150,
+    height: 31,
+  }
 });
